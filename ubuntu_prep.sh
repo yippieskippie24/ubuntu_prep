063d1e5848e2dcb73efe6dfa79c611bac5e093e7
yippieskippie24 <yippieskippie24@gmail.com>
2016-11-11 17:37:03 -0600

    Initial script creation

---
+++ ubuntu_prep.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+#                                                         #
+#                  Ubuntu Prep Script                     #
+#                                                         #
+#    This script will allow you to quickly prepare        #
+#             a newly install Ubuntu server               #
+#                                                         #
+#              Written by:  Tyler M Johnson               #
+#           https://github.com/yippieskippie24            #
+#                                                         #
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
